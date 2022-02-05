560eb0202efc5da2e46fe96f3fcdf2083edcb527
Naveen <navee9900@gmail.com>
2022-02-05 15:07:54 +0530

    adding EMS src to repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeesComponent } from './add-employees/add-employees.component';
+import { EditEmployeesComponent } from './edit-employees/edit-employees.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"emp",component:EmployeesComponent},
+  {path:"addEmp",component:AddEmployeesComponent},
+  {path:"editEmp",component:EditEmployeesComponent},
+  {path:"login",component:LoginComponent},
+  {path:"regn",component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AddEmployeesComponent } from './add-employees/add-employees.component';
+import { EditEmployeesComponent } from './edit-employees/edit-employees.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    AddEmployeesComponent,
+    EditEmployeesComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
