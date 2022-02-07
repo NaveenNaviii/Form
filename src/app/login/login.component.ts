ef71b5f602277ccce4c82d0140af2281f85add86
NAZEER143 <nazeer.p.nadaf92@gmail.com>
2022-02-07 13:24:24 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  loginForm=new FormGroup({
+    email:new FormControl("",[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
+    password:new FormControl("",[Validators.required,Validators.pattern("(?=^.{8,}$)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$")]) 
+  })
+  get email(){
+    return this.loginForm.get("email")
+  }
+  get password(){
+    return this.loginForm.get("password")
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    
+    
+    return console.log(this.loginForm.value);
+  
+
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
